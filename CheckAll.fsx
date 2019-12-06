c19ad09b417a01196ddf00b6b9778297da2918da
Heikki Vesterinen <hvesteri@gmail.com>
2019-12-06 16:35:11 +0200

    CheckAll.fsx added

---
+++ CheckAll.fsx
@@ -0,0 +1,23 @@
+#load "Day1.fsx"
+#load "Day2.fsx"
+#load "Day3.fsx"
+#load "Day4.fsx"
+#load "Day5.fsx"
+
+let check result expected =
+    if result <> expected then
+        failwithf "Result %A does not equal %A" result expected 
+
+check Day1.result1 3329926
+check Day1.result2 4992008
+
+check Day2.result1 3716250
+check Day2.result2 6472
+
+check Day3.result1 293
+check Day3.result2 27306
+
+check Day4.result1 1890
+check Day4.result2 1277
+
+check Day5.result1 5577461
