e1771b8bd19dbbda7c12df1a105b8fe0f20749ac
Heikki Vesterinen <hvesteri@gmail.com>
2019-12-06 11:19:06 +0200

    Interpreter code to separate file

--- Day2.fsx
+++ Day2.fsx
@@ -1,33 +1,12 @@
+#load "Day1.fsx"
+#load "Interpreter.fsx"
+
 open System.IO
+open Interpreter
 
 let inputProgram =
     File.ReadAllText("data/input2.txt").Split([|','|])
     |> Array.map int
-    
-let getOperands (memory : int array) i =
-    let outputPosition = memory.[i + 3]
-    let operand1 = memory.[memory.[i + 1]]
-    let operand2 = memory.[memory.[i + 2]]
-    outputPosition, operand1, operand2
-
-let runProgram program input1 input2 =
-    let memory = Array.copy program
-    memory.[1] <- input1
-    memory.[2] <- input2
-    let rec loop i =
-        match memory.[i] with
-        | 99 -> memory
-        | 1 ->
-            let outputPosition, operand1, operand2 = getOperands memory i
-            memory.[outputPosition] <- operand1 + operand2
-            loop (i + 4)
-        | 2 ->
-            let outputPosition, operand1, operand2 = getOperands memory i
-            memory.[outputPosition] <- operand1 * operand2
-            loop (i + 4)
-        | _ ->
-            failwith "Invalid op code"
-    loop 0
 
 let result1 = runProgram inputProgram 12 2 |> Array.item 0 // 3716250
 
